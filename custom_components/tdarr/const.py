5c11f967623162bef23a900eb9696a585bb99c35
Deosrc <deosrc@g-de.co.uk>
2025-02-26 21:52:25 +0000

    Move sensor icons and unit of measurement to entity descriptions

--- custom_components/tdarr/const.py
+++ custom_components/tdarr/const.py
@@ -8,18 +8,18 @@ COORDINATOR = "coordinator"
 APIKEY = "apikey"
 
 SENSORS = {
-    "server": {"icon": "mdi:server", "type": "single", "entry": "server"},
-    "stats_spacesaved": {"icon": "mdi:harddisk","type": "single", "entry": "stats", "unit_of_measurement": "GB", "device_class": "data_size"},
-    "stats_transcodefilesremaining": {"icon": "mdi:file-multiple", "unit_of_measurement": "Files", "type": "single", "entry": "stats"},
-    "stats_transcodedcount": {"icon": "mdi:file-multiple", "unit_of_measurement": "Files", "type": "single", "entry": "stats"},
-    "stats_stagedcount": {"icon": "mdi:file-multiple", "unit_of_measurement": "Files", "type": "single", "entry": "staged"},
-    "stats_healthcount": {"icon": "mdi:file-multiple", "unit_of_measurement": "Files", "type": "single", "entry": "stats"},
-    "stats_transcodeerrorcount": {"icon": "mdi:file-multiple", "unit_of_measurement": "Files", "type": "single", "entry": "stats"},
-    "stats_healtherrorcount": {"icon": "mdi:medication-outline", "unit_of_measurement": "Files", "type": "single", "entry": "stats"},
-    "node": {"icon": "mdi:server-network-outline"},
-    "nodefps": {"icon": "mdi:video", "unit_of_measurement": "FPS"},
-    "stats_totalfps": {"icon": "mdi:video", "unit_of_measurement": "FPS", "type": "single", "entry": "nodes"},
-    "library": {"icon": "mdi:folder-multiple", "unit_of_measurement": "Files"},
+    "server": {"type": "single", "entry": "server"},
+    "stats_spacesaved": {"entry": "stats", "device_class": "data_size"},
+    "stats_transcodefilesremaining": {"type": "single", "entry": "stats"},
+    "stats_transcodedcount": {"type": "single", "entry": "stats"},
+    "stats_stagedcount": {"type": "single", "entry": "staged"},
+    "stats_healthcount": {"type": "single", "entry": "stats"},
+    "stats_transcodeerrorcount": {"type": "single", "entry": "stats"},
+    "stats_healtherrorcount": {"type": "single", "entry": "stats"},
+    "node": {},
+    "nodefps": {},
+    "stats_totalfps": {"type": "single", "entry": "nodes"},
+    "library": {},
 }
 
 SWITCHES = {
--- custom_components/tdarr/sensor.py
+++ custom_components/tdarr/sensor.py
@@ -3,6 +3,7 @@ import re
 
 from homeassistant.components.sensor import (
     SensorEntity,
+    SensorEntityDescription,
     SensorDeviceClass,
     SensorStateClass
 )
@@ -12,6 +13,71 @@ from .const import DOMAIN, COORDINATOR, SENSORS
 
 _LOGGER = logging.getLogger(__name__)
 
+SERVER_ENTITY_DESCRIPTIONS = {
+    "server": SensorEntityDescription(
+        key="server",
+        icon="mdi:server"
+    ),
+    "stats_spacesaved": SensorEntityDescription(
+        key="stats_spacesaved",
+        icon="mdi:harddisk",
+        unit_of_measurement="GB"
+    ),
+    "stats_transcodefilesremaining": SensorEntityDescription(
+        key="stats_transcodefilesremaining",
+        icon="mdi:file-multiple",
+        unit_of_measurement="Files"
+    ),
+    "stats_transcodedcount": SensorEntityDescription(
+        key="stats_transcodedcount",
+        icon="mdi:file-multiple",
+        unit_of_measurement="Files"
+    ),
+    "stats_stagedcount": SensorEntityDescription(
+        key="stats_stagedcount",
+        icon="mdi:file-multiple",
+        unit_of_measurement="Files"
+    ),
+    "stats_healthcount": SensorEntityDescription(
+        key="stats_healthcount",
+        icon="mdi:file-multiple",
+        unit_of_measurement="Files"
+    ),
+    "stats_transcodeerrorcount": SensorEntityDescription(
+        key="stats_transcodeerrorcount",
+        icon="mdi:file-multiple",
+        unit_of_measurement="Files"
+    ),
+    "stats_healtherrorcount": SensorEntityDescription(
+        key="stats_healtherrorcount",
+        icon="mdi:medication-outline",
+        unit_of_measurement="Files"
+    ),
+    "stats_totalfps": SensorEntityDescription(
+        key="stats_totalfps",
+        icon="mdi:video",
+        unit_of_measurement="FPS"
+    ),
+}
+
+LIBRARY_ENTITY_DESCRIPTION=SensorEntityDescription(
+    key="library",
+    icon="mdi:folder-multiple",
+    unit_of_measurement="Files"
+)
+
+NODE_ENTITY_DESCRIPTIONS={
+    "node": SensorEntityDescription(
+        key="node",
+        icon="mdi:server-network-outline",
+    ),
+    "nodefps": SensorEntityDescription(
+        key="node",
+        icon="mdi:video",
+        unit_of_measurement="FPS"
+    )
+}
+
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Add the Entities from the config."""
     entry = hass.data[DOMAIN][config_entry.entry_id][COORDINATOR]
@@ -20,18 +86,18 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     #_LOGGER.debug(entry.data)
     for key, value in SENSORS.items():
         if value.get("type", "") == "single":
-            sensors.append(TdarrSensor(entry, entry.data[value["entry"]], config_entry.options, key))
+            sensors.append(TdarrSensor(entry, entry.data[value["entry"]], config_entry.options, key, SERVER_ENTITY_DESCRIPTIONS[key]))
     # Server Library Sensors
     id = 0
     for value in entry.data["libraries"]:
         #value.insert(0, id)
-        sensors.append(TdarrSensor(entry, value, config_entry.options, "library"))
+        sensors.append(TdarrSensor(entry, value, config_entry.options, "library", LIBRARY_ENTITY_DESCRIPTION))
         id += 1
     # Server Node Sensors
     fps_count = 0
     for key, value in entry.data["nodes"].items():
-        sensors.append(TdarrSensor(entry, value, config_entry.options, "node"))
-        sensors.append(TdarrSensor(entry, value, config_entry.options, "nodefps"))
+        sensors.append(TdarrSensor(entry, value, config_entry.options, "node", NODE_ENTITY_DESCRIPTIONS["node"]))
+        sensors.append(TdarrSensor(entry, value, config_entry.options, "nodefps", NODE_ENTITY_DESCRIPTIONS["nodefps"]))
     
 
     async_add_entities(sensors, True)
@@ -41,11 +107,12 @@ class TdarrSensor(
     TdarrEntity,
     SensorEntity,
 ):
-    def __init__(self, coordinator, sensor, options, type):
+    def __init__(self, coordinator, sensor, options, type, entity_description: SensorEntityDescription):
 
         self.sensor = sensor
         self.tdarroptions = options
         self.type = type
+        self.entity_description = entity_description
         self._attr = {}
         self.coordinator = coordinator
         if self.type == "server":
@@ -171,18 +238,10 @@ class TdarrSensor(
     @property
     def extra_state_attributes(self):
         return self.get_value("attributes")
-
-    @property
-    def native_unit_of_measurement(self):
-        return SENSORS.get(self.type, {}).get("unit_of_measurement", None)
-
+    
     @property
     def device_class(self):
         return SENSORS.get(self.type, {}).get("device_class", None)
-
-    @property
-    def icon(self):
-        return SENSORS.get(self.type, {}).get("icon", None)
     
     @property
     def state_class(self):

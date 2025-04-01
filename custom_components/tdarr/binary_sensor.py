686728b5a1ff4186d4a4daef3ff4e87180cefd02
deosrc <deosrc@g-de.co.uk>
2025-04-01 21:37:22 +0100

    Replace node status sensor with binary sensor (#33)

---
+++ custom_components/tdarr/binary_sensor.py
@@ -0,0 +1,99 @@
+from dataclasses import dataclass
+import logging
+from typing import Callable
+
+from homeassistant.components.binary_sensor import (
+    BinarySensorEntity,
+    BinarySensorEntityDescription,
+    BinarySensorDeviceClass,
+)
+
+from . import (
+    TdarrServerEntity,
+    TdarrNodeEntity,
+)
+from .coordinator import TdarrDataUpdateCoordinator
+from .const import (
+    DOMAIN,
+    COORDINATOR,
+)
+
+_LOGGER = logging.getLogger(__name__)
+
+@dataclass(frozen=True, kw_only=True)
+class TdarrBinarySensorEntityDescription(BinarySensorEntityDescription):
+    """Details of a Tdarr sensor entity"""
+
+    value_fn: Callable[[dict], bool | None]
+    attributes_fn: Callable[[dict], dict | None] = None
+
+SERVER_ENTITY_DESCRIPTIONS = {
+}
+
+NODE_ENTITY_DESCRIPTIONS = {
+    TdarrBinarySensorEntityDescription(
+        key="status",
+        translation_key="status",
+        icon="mdi:server-network-outline",
+        device_class=BinarySensorDeviceClass.CONNECTIVITY,
+        value_fn=lambda data: bool(data),
+    ),
+}
+
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    """Add the Entities from the config."""
+    entry = hass.data[DOMAIN][config_entry.entry_id][COORDINATOR]
+    sensors = []
+
+    # Server Binary Sensors
+    for description in SERVER_ENTITY_DESCRIPTIONS:
+        sensors.append(TdarrServerBinarySensor(entry, config_entry.options, description))
+
+    # Node Binary Sensors
+    for node_id in entry.data["nodes"]:
+        for description in NODE_ENTITY_DESCRIPTIONS:
+            sensors.append(TdarrNodeBinarySensor(entry, node_id, config_entry.options, description))
+
+    async_add_entities(sensors, True)
+
+
+class TdarrServerBinarySensor(TdarrServerEntity, BinarySensorEntity):
+
+    def __init__(self, coordinator: TdarrDataUpdateCoordinator, options, entity_description: TdarrBinarySensorEntityDescription):
+        _LOGGER.info("Creating server level binary sensor %s", entity_description.key)
+        super().__init__(coordinator, entity_description)
+
+    @property
+    def description(self) -> TdarrBinarySensorEntityDescription:
+        return self.entity_description
+
+    @property
+    def is_on(self):
+        return self.description.value_fn(self.data)
+
+    @property
+    def extra_state_attributes(self):
+        if self.description.attributes_fn:
+            return self.description.attributes_fn(self.data)
+
+
+class TdarrNodeBinarySensor(TdarrNodeEntity, BinarySensorEntity):
+
+    def __init__(self, coordinator: TdarrDataUpdateCoordinator, node_key: str, options, entity_description: TdarrBinarySensorEntityDescription):
+        _LOGGER.info("Creating node %s level binary sensor %s", node_key, entity_description.key)
+        super().__init__(coordinator, node_key, entity_description)
+
+    @property
+    def description(self) -> TdarrBinarySensorEntityDescription:
+        return self.entity_description
+
+    @property
+    def is_on(self):
+        return self.description.value_fn(self.data)
+
+    @property
+    def extra_state_attributes(self):
+        if self.description.attributes_fn:
+            return self.description.attributes_fn(self.data)
+        else:
+            return self.data

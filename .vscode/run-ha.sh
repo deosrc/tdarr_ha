66cf80b44abcf6d293b274c45cf78f938fd1c59c
Deosrc <deosrc@g-de.co.uk>
2025-04-05 22:37:06 +0100

    Add blueprint folders to HA container

--- .vscode/run-ha.sh
+++ .vscode/run-ha.sh
@@ -14,6 +14,8 @@ docker run --rm \
     -v $(pwd)/.data:/config:rw \
     -v $(pwd)/debug_configuration.yaml:/config/configuration.yaml:ro \
     -v $(pwd)/custom_components:/config/custom_components:ro \
+    -v $(pwd)/blueprints/script:/config/blueprints/script/tdarr:ro \
+    -v $(pwd)/blueprints/automation:/config/blueprints/automation/tdarr:ro \
     --user 1000:1000 \
     --name homeassistant \
     homeassistant/home-assistant:2025.3.4
